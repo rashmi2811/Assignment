3cf405f624b5c5bb2d9f688489715317279b5d26
Rashmi <rashmi214282@gmail.com>
2022-12-02 16:32:40 +0530

    Added lastnumlines

--- c_assignment/firstnumlines.c
+++ c_assignment/firstnumlines.c
@@ -1,29 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char ** firstnumlines(FILE * inputfp, unsigned int num);
 
-int main()
-{
-   FILE * fp = fopen("file.txt", "r");
-   // Assume fp is not NULL
-   char ** firstlines = firstnumlines(fp, 10);
-   for (int i = 0; i < 10; i++) {
-        if (firstlines[i] != NULL) {
-            printf("%s", firstlines[i]);
-        }
-   }
-   printf("\n");
-   fclose(fp);
-   free(firstlines);
-}
+char ** firstnumlines(FILE * inputfp, unsigned int num){
+	char ** headbuff = (char **) calloc(num, sizeof(char *));
+	int i=0;
+	int j=1;
+	while(j<=num){
 
-char ** firstnumlines(FILE * inputfp, unsigned int num)
-{
-	char ** headbuff = calloc(num, sizeof(char *));
-	for(unsigned int i = 0; i < num; i++) {
-		   headbuff[i] = (char *) calloc(255, sizeof(char));
-		   fgets(headbuff[i],255,inputfp);
+		if(feof(inputfp)){
+			printf("\nGiven Size is Greater than the input file\n\n");
+			for(int i =0;i<num;i++){
+				free(headbuff[i]);
+			}
+			free(headbuff);
+
+			return NULL;
+		}
+		else{
+			headbuff[i] = (char *) calloc(255, sizeof(char));
+			fgets(headbuff[i],255,inputfp);
+			i++;
+			j++;
+		}
 	}
 	return headbuff;
+
+}
+
+
+
+int main() {
+	FILE * inputfp = fopen("input.txt", "r");
+
+	int num;
+	scanf("%d",&num);
+	printf("\n");
+
+	char ** firstlines = firstnumlines(inputfp,num);
+
+	if(firstlines==NULL){
+		return 0;
+	}
+
+	for(int i=0;i<num;i++){
+		if(firstlines[i] != NULL){
+			printf("%s",firstlines[i]);
+		}
+	}
+
+	for(int i =0;i<num;i++){
+		free(firstlines[i]);
+	}
+	free(firstlines);
+
+	fclose(inputfp);
+
 }
---
+++ c_assignment/lastnumlines.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char ** lastnumlines(FILE * inputfp, unsigned int num){
+	char ** tailbuff = (char **) calloc(num, sizeof(char *));
+	char ** temp = (char **) calloc(num, sizeof(char *));
+	int i=0;
+	int j=1;
+	while(j<=num){
+
+		if(feof(inputfp)){
+			printf("\nGiven Size is Greater than the input file\n\n");
+			for(int i =0;i<num;i++){
+				free(temp[i]);
+			}
+			free(temp);
+			free(tailbuff);
+
+			return NULL;
+		}
+		else{
+			temp[i] = (char *) calloc(255, sizeof(char));
+			fgets(temp[i],255,inputfp);
+			i++;
+			j++;
+		}
+	}
+
+	int lines = sizeof(temp)/sizeof(temp[0]);
+
+	for(int a = 0, b = lines-num; b < num; a++, b++) {
+		tailbuff[a] = (char *) calloc(255, sizeof(char));
+		tailbuff[a] = temp[b];
+	}
+	return tailbuff;
+
+}
+
+
+
+int main() {
+	FILE * inputfp = fopen("input.txt", "r");
+
+	int num;
+	scanf("%d",&num);
+	printf("\n");
+
+	char ** lastlines = lastnumlines(inputfp,num);
+
+	if(lastlines==NULL){
+		return 0;
+	}
+
+	for(int i=0;i<num;i++){
+		if(lastlines[i] != NULL){
+			printf("%s",lastlines[i]);
+		}
+	}
+
+	for(int i =0;i<num;i++){
+		free(lastlines[i]);
+	}
+	free(lastlines);
+
+	fclose(inputfp);
+
+}
