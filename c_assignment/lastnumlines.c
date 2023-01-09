52974aa446f2ec9a28e29bcc837bc795dafa1e90
Rashmi <rashmi214282@gmail.com>
2023-01-09 17:54:49 +0530

    Daytime client server

--- c_assignment/lastnumlines.c
+++ c_assignment/lastnumlines.c
@@ -4,71 +4,53 @@
 
 char ** lastnumlines(FILE * inputfp, unsigned int num){
 
-	if(num <= 0)
-		return NULL;
-
 	char ** tailbuff = (char **) calloc(num, sizeof(char *));
-	char ** temp = (char **) calloc(num, sizeof(char *));
 
 	for(int j = 0; j < num; j++) {
 		tailbuff[j] = (char *) calloc(255, sizeof(char));
-		temp[j] = (char *) calloc(255, sizeof(char));
 	}
 
 	int i = 0, lines = 0;
-	//printf("lines - i - tailbuff\n");
 	while(!feof(inputfp)){
 
 			if(lines % num == 0)
 				i = 0;
-
-			fgets(temp[i], 255, inputfp);
-			//printf("%d     - %d     - %s\n", lines, i, temp[i]);
-
-			//bcopy(fgets(tailbuff[i], 255, inputfp), tailbuff[i], sizeof(tailbuff[i]));
-
-			//printf("%d %p\n",i, &temp[i]);
+			fgets(tailbuff[i], 255, inputfp);
 			i++;
 			lines++;
 	}
-
-	//printf("%d\n",lines);
-	//printf("%d\n",i);
-	
+	//printf("%d\n\n",i);	
 	if(num >= lines){
-		printf("Given input exceeds file size\n");
 		for(int  j = 0; j < num; j++){
-			free(temp[j]);
 			free(tailbuff[j]);
 		}
-		free(temp);
 		free(tailbuff);
 		return NULL;
 	}
 
-	for(int a = 0, b = i-1; b < num; a++, b++)
-		strcpy(tailbuff[a], temp[b]);
-
-	for(int a = i, b = 0; b < i-1; a++, b++)
-		strcpy(tailbuff[a], temp[b]);
-
-	for(int j = 0; j < num; j++){
-		free(temp[j]);
+	for(int a = 0; a < i-1; a++) {
+		char * temp = tailbuff[0];
+		for(int b = 0; b < num-1; b++) {
+			tailbuff[b] = tailbuff[b+1];
+		}
+		tailbuff[num-1] = temp;
 	}
-
-	free(temp);
 	
 	return tailbuff;
-
 }
 
 
-int main(int argc, char ** argv) {
+int main(int argc, char ** argv) {	
 	FILE * inputfp = fopen(argv[1], "r");
 
 	int num;
 	scanf("%d",&num);
 	printf("\n");
+
+	if(num <= 0){
+		fclose(inputfp);
+		return 0;
+	}
 
 	char ** lastlines = lastnumlines(inputfp,num);
 
@@ -80,8 +62,6 @@ int main(int argc, char ** argv) {
 	for(int i=0;i<num;i++){
 		if(lastlines[i] != NULL){
 		printf("%s",lastlines[i]);
-		
-		//printf("%d %p\n",i, &lastlines[i]);
 		}
 	}
 
---
+++ networking/unp.h
@@ -0,0 +1,94 @@
+#include	<sys/types.h>	/* basic system data types */
+#include	<sys/socket.h>	/* basic socket definitions */
+#include	<sys/time.h>	/* timeval{} for select() */
+#include	<time.h>		/* timespec{} for pselect() */
+#include	<netinet/in.h>	/* sockaddr_in{} and other Internet defns */
+#include	<arpa/inet.h>	/* inet(3) functions */
+#include	<errno.h>
+#include	<fcntl.h>		/* for nonblocking */
+#include	<netdb.h>
+#include	<signal.h>
+#include	<stdio.h>
+#include	<stdlib.h>
+#include	<string.h>
+#include	<sys/stat.h>	/* for S_xxx file mode constants */
+#include	<sys/uio.h>		/* for iovec{} and readv/writev */
+#include	<unistd.h>
+#include	<sys/wait.h>
+#include	<sys/un.h>
+#include	<errno.h>
+
+#define MAXLINE 4096
+#define SA struct sockaddr
+#define LISTENQ 1024
+
+int Socket(int family, int type, int protocol) {
+	int n;
+	if((n = socket(family,type,protocol)) < 0) {
+		perror("[-]socket() Error");
+		exit(1);
+	}
+	printf("[+]Socket Created\n");
+	return n;
+}
+
+int Connect(int sockfd, SA *addr, socklen_t addrlen) {
+       int n;
+       if((n = connect(sockfd, addr, addrlen)) < 0) {
+	       perror("[-]connect() Error");
+	       exit(1);
+       }
+       printf("[+]Connection established\n");
+       return n;
+}
+
+int Bind(int sockfd, SA *addr, socklen_t addrlen) {
+	int n;
+	if((n = bind(sockfd, addr, addrlen)) < 0) {
+		perror("[-]bind() Error");
+		exit(1);
+	}
+	printf("[+]Bind Successful\n");
+	return n;
+}
+
+int Listen(int sockfd, int listenq) {
+	int n;
+	if((n = listen(sockfd, listenq)) < 0) {
+		perror("[-]listen() Error");
+		exit(1);
+	}
+	printf("[+]Listening for the client...\n");
+	return n;
+}
+
+int Accept(int sockfd, SA *addr, socklen_t *addrlen) {
+	int n;
+	if((n = accept(sockfd, addr, addrlen)) < 0) {
+		perror("[-]accept() Error");
+		exit(1);
+	}
+	printf("[+]Client Connected\n");
+	return n;
+}
+
+ssize_t Write(int sockfd, void *buff, size_t count) {
+	ssize_t n;
+	if((n = write(sockfd, buff, count)) < 0) {
+		perror("[-]write() Error");
+		exit(1);
+	}
+	printf("[+]Write implemented\n");
+	return n;
+}
+
+ssize_t Read(int sockfd, void *buff, size_t count) {
+	ssize_t n;
+	if((n = read(sockfd, buff, count)) < 0) {
+		perror("[-]read() Error");
+		exit(1);
+	}
+	if(n != 0)
+	printf("[+]Read Successful\n");
+	return n;
+}
